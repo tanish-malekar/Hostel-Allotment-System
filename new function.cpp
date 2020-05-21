46d4859af97627c5f298924d4859951d488a303e
Adya Agrawal <56762107+adyaagrawal@users.noreply.github.com>
2020-05-21 19:31:53 +0530

    new function with + if statements

---
+++ new function.cpp
@@ -0,0 +1,240 @@
+ if((b=='A')&&(r=="1AC")&&(Block[0].AC1>0))
+        Block[0].AC1 += 1;
+    else if((b=='A')&&(r=="1NAC")&&(Block[0].NAC1>0))
+        Block[0].NAC1 += 1;
+    else if((b=='A')&&(r=="2AC")&&(Block[0].AC2>0))
+        Block[0].AC2 += 1;
+    else if((b=='A')&&(r=="2NAC")&&(Block[0].NAC2>0))
+        Block[0].NAC2 += 1;
+    else if((b=='A')&&(r=="3AC")&&(Block[0].AC3>0))
+        Block[0].AC3 += 1;
+    else if((b=='A')&&(r=="3NAC")&&(Block[0].NAC3>0))
+        Block[0].NAC3 += 1;
+    else if((b=='A')&&(r=="4AC")&&(Block[0].AC4>0))
+        Block[0].AC4 += 1;
+    else if((b=='A')&&(r=="4NAC")&&(Block[0].NAC4>0))
+        Block[0].NAC4 += 1;
+    else if((b=='A')&&(r=="6AC")&&(Block[0].AC6>0))
+        Block[0].AC6 += 1;
+    else if((b=='A')&&(r=="6NAC")&&(Block[0].NAC6>0))
+        Block[0].NAC6 += 1;
+    else if((b=='B')&&(r=="1AC")&&(Block[1].AC1>0))
+        Block[1].AC1 += 1;
+    else if((b=='B')&&(r=="1NAC")&&(Block[1].NAC1>0))
+        Block[1].NAC1 += 1;
+    else if((b=='B')&&(r=="2AC")&&(Block[1].AC2>0))
+        Block[1].AC2 += 1;
+    else if((b=='B')&&(r=="2NAC")&&(Block[1].NAC2>0))
+        Block[1].NAC2 += 1;
+    else if((b=='B')&&(r=="3AC")&&(Block[1].AC3>0))
+        Block[1].AC3 += 1;
+    else if((b=='B')&&(r=="3NAC")&&(Block[1].NAC3>0))
+        Block[1].NAC3 += 1;
+    else if((b=='B')&&(r=="4AC")&&(Block[1].AC4>0))
+        Block[1].AC4 += 1;
+    else if((b=='B')&&(r=="4NAC")&&(Block[1].NAC4>0))
+        Block[1].NAC4 += 1;
+    else if((b=='B')&&(r=="6AC")&&(Block[1].AC6>0))
+        Block[1].AC6 += 1;
+    else if((b=='B')&&(r=="6NAC")&&(Block[1].NAC6>0))
+        Block[1].NAC6 += 1;
+   else if((b=='C')&&(r=="1AC")&&(Block[2].AC1>0))
+        Block[2].AC1 += 1;
+    else if((b=='C')&&(r=="1NAC")&&(Block[2].NAC1>0))
+        Block[2].NAC1 += 1;
+    else if((b=='C')&&(r=="2AC")&&(Block[2].AC2>0))
+        Block[2].AC2 += 1;
+    else if((b=='C')&&(r=="2NAC")&&(Block[2].NAC2>0))
+        Block[2].NAC2 += 1;
+    else if((b=='C')&&(r=="3AC")&&(Block[2].AC3>0))
+        Block[2].AC3 += 1;
+    else if((b=='C')&&(r=="3NAC")&&(Block[2].NAC3>0))
+        Block[2].NAC3 += 1;
+    else if((b=='C')&&(r=="4AC")&&(Block[2].AC4>0))
+        Block[2].AC4 += 1;
+    else if((b=='C')&&(r=="4NAC")&&(Block[2].NAC4>0))
+        Block[2].NAC4 += 1;
+    else if((b=='C')&&(r=="6AC")&&(Block[2].AC6>0))
+        Block[2].AC6 += 1;
+    else if((b=='C')&&(r=="6NAC")&&(Block[2].NAC6>0))
+        Block[2].NAC6 += 1;
+    else if((b=='D')&&(r=="1AC")&&(Block[3].AC1>0))
+        Block[3].AC1 += 1;
+    else if((b=='D')&&(r=="1NAC")&&(Block[3].NAC1>0))
+        Block[3].NAC1 += 1;
+    else if((b=='D')&&(r=="2AC")&&(Block[3].AC2>0))
+        Block[3].AC2 += 1;
+    else if((b=='D')&&(r=="2NAC")&&(Block[3].NAC2>0))
+        Block[3].NAC2 += 1;
+    else if((b=='D')&&(r=="3AC")&&(Block[3].AC3>0))
+        Block[3].AC3 += 1;
+    else if((b=='D')&&(r=="3NAC")&&(Block[3].NAC3>0))
+        Block[3].NAC3 += 1;
+    else if((b=='D')&&(r=="4AC")&&(Block[3].AC4>0))
+        Block[3].AC4 += 1;
+    else if((b=='D')&&(r=="4NAC")&&(Block[3].NAC4>0))
+        Block[3].NAC4 += 1;
+    else if((b=='D')&&(r=="6AC")&&(Block[3].AC6>0))
+        Block[3].AC6 += 1;
+    else if((b=='D')&&(r=="6NAC")&&(Block[3].NAC6>0))
+        Block[3].NAC6 += 1;
+    else if((b=='E')&&(r=="1AC")&&(Block[4].AC1>0))
+        Block[4].AC1 += 1;
+    else if((b=='E')&&(r=="1NAC")&&(Block[4].NAC1>0))
+        Block[4].NAC1 += 1;
+    else if((b=='E')&&(r=="2AC")&&(Block[4].AC2>0))
+        Block[4].AC2 += 1;
+    else if((b=='E')&&(r=="2NAC")&&(Block[4].NAC2>0))
+        Block[4].NAC2 += 1;
+    else if((b=='E')&&(r=="3AC")&&(Block[4].AC3>0))
+        Block[4].AC3 += 1;
+    else if((b=='E')&&(r=="3NAC")&&(Block[4].NAC3>0))
+        Block[4].NAC3 += 1;
+    else if((b=='E')&&(r=="4AC")&&(Block[4].AC4>0))
+        Block[4].AC4 += 1;
+    else if((b=='E')&&(r=="4NAC")&&(Block[4].NAC4>0))
+        Block[4].NAC4 += 1;
+    else if((b=='E')&&(r=="6AC")&&(Block[4].AC6>0))
+        Block[4].AC6 += 1;
+    else if((b=='E')&&(r=="6NAC")&&(Block[4].NAC6>0))
+        Block[4].NAC6 += 1;
+    else if((b=='F')&&(r=="1AC")&&(Block[5].AC1>0))
+        Block[5].AC1 += 1;
+    else if((b=='F')&&(r=="1NAC")&&(Block[5].NAC1>0))
+        Block[5].NAC1 += 1;
+    else if((b=='F')&&(r=="2AC")&&(Block[5].AC2>0))
+        Block[5].AC2 += 1;
+    else if((b=='F')&&(r=="2NAC")&&(Block[5].NAC2>0))
+        Block[5].NAC2 += 1;
+    else if((b=='F')&&(r=="3AC")&&(Block[5].AC3>0))
+        Block[5].AC3 += 1;
+    else if((b=='F')&&(r=="3NAC")&&(Block[5].NAC3>0))
+        Block[5].NAC3 += 1;
+    else if((b=='F')&&(r=="4AC")&&(Block[5].AC4>0))
+        Block[5].AC4 += 1;
+    else if((b=='F')&&(r=="4NAC")&&(Block[5].NAC4>0))
+        Block[5].NAC4 += 1;
+    else if((b=='F')&&(r=="6AC")&&(Block[5].AC6>0))
+        Block[5].AC6 += 1;
+    else if((b=='F')&&(r=="6NAC")&&(Block[5].NAC6>0))
+        Block[5].NAC6 += 1;
+     else if((b=='G')&&(r=="1AC")&&(Block[6].AC1>0))
+        Block[6].AC1 += 1;
+    else if((b=='G')&&(r=="1NAC")&&(Block[6].NAC1>0))
+        Block[6].NAC1 += 1;
+    else if((b=='G')&&(r=="2AC")&&(Block[6].AC2>0))
+        Block[6].AC2 += 1;
+    else if((b=='G')&&(r=="2NAC")&&(Block[6].NAC2>0))
+        Block[6].NAC2 += 1;
+    else if((b=='G')&&(r=="3AC")&&(Block[6].AC3>0))
+        Block[6].AC3 += 1;
+    else if((b=='G')&&(r=="3NAC")&&(Block[6].NAC3>0))
+        Block[6].NAC3 += 1;
+    else if((b=='G')&&(r=="4AC")&&(Block[6].AC4>0))
+        Block[6].AC4 += 1;
+    else if((b=='G')&&(r=="4NAC")&&(Block[6].NAC4>0))
+        Block[6].NAC4 += 1;
+    else if((b=='G')&&(r=="6AC")&&(Block[6].AC6>0))
+        Block[6].AC6 += 1;
+    else if((b=='G')&&(r=="6NAC")&&(Block[6].NAC6>0))
+        Block[6].NAC6 += 1;
+    else if((b=='H')&&(r=="1AC")&&(Block[7].AC1>0))
+        Block[7].AC1 += 1;
+    else if((b=='H')&&(r=="1NAC")&&(Block[7].NAC1>0))
+        Block[7].NAC1 += 1;
+    else if((b=='H')&&(r=="2AC")&&(Block[7].AC2>0))
+        Block[7].AC2 += 1;
+    else if((b=='H')&&(r=="2NAC")&&(Block[7].NAC2>0))
+        Block[7].NAC2 += 1;
+    else if((b=='H')&&(r=="3AC")&&(Block[7].AC3>0))
+        Block[7].AC3 += 1;
+    else if((b=='H')&&(r=="3NAC")&&(Block[7].NAC3>0))
+        Block[7].NAC3 += 1;
+    else if((b=='H')&&(r=="4AC")&&(Block[7].AC4>0))
+        Block[7].AC4 += 1;
+    else if((b=='H')&&(r=="4NAC")&&(Block[7].NAC4>0))
+        Block[7].NAC4 += 1;
+    else if((b=='H')&&(r=="6AC")&&(Block[7].AC6>0))
+        Block[7].AC6 += 1;
+    else if((b=='H')&&(r=="6NAC")&&(Block[7].NAC6>0))
+        Block[7].NAC6 += 1;
+    else if((b=='J')&&(r=="1AC")&&(Block[8].AC1>0))
+        Block[8].AC1 += 1;
+    else if((b=='J')&&(r=="1NAC")&&(Block[8].NAC1>0))
+        Block[8].NAC1 += 1;
+    else if((b=='J')&&(r=="2AC")&&(Block[8].AC2>0))
+        Block[8].AC2 += 1;
+    else if((b=='J')&&(r=="2NAC")&&(Block[8].NAC2>0))
+        Block[8].NAC2 += 1;
+    else if((b=='J')&&(r=="3AC")&&(Block[8].AC3>0))
+        Block[8].AC3 += 1;
+    else if((b=='J')&&(r=="3NAC")&&(Block[8].NAC3>0))
+        Block[8].NAC3 += 1;
+    else if((b=='J')&&(r=="4AC")&&(Block[8].AC4>0))
+        Block[8].AC4 += 1;
+    else if((b=='J')&&(r=="4NAC")&&(Block[8].NAC4>0))
+        Block[8].NAC4 += 1;
+    else if((b=='J')&&(r=="6AC")&&(Block[8].AC6>0))
+        Block[8].AC6 += 1;
+    else if((b=='J')&&(r=="6NAC")&&(Block[8].NAC6>0))
+        Block[8].NAC6 += 1;
+    else if((b=='K')&&(r=="1AC")&&(Block[9].AC1>0))
+        Block[9].AC1 += 1;
+    else if((b=='K')&&(r=="1NAC")&&(Block[9].NAC1>0))
+        Block[9].NAC1 += 1;
+    else if((b=='K')&&(r=="2AC")&&(Block[9].AC2>0))
+        Block[9].AC2 += 1;
+    else if((b=='K')&&(r=="2NAC")&&(Block[9].NAC2>0))
+        Block[9].NAC2 += 1;
+    else if((b=='K')&&(r=="3AC")&&(Block[9].AC3>0))
+        Block[9].AC3 += 1;
+    else if((b=='K')&&(r=="3NAC")&&(Block[9].NAC3>0))
+        Block[9].NAC3 += 1;
+    else if((b=='K')&&(r=="4AC")&&(Block[9].AC4>0))
+        Block[9].AC4 += 1;
+    else if((b=='K')&&(r=="4NAC")&&(Block[9].NAC4>0))
+        Block[9].NAC4 += 1;
+    else if((b=='K')&&(r=="6AC")&&(Block[9].AC6>0))
+        Block[9].AC6 += 1;
+    else if((b=='K')&&(r=="6NAC")&&(Block[9].NAC6>0))
+        Block[9].NAC6 += 1;
+    else if((b=='L')&&(r=="1AC")&&(Block[10].AC1>0))
+        Block[10].AC1 += 1;
+    else if((b=='L')&&(r=="1NAC")&&(Block[10].NAC1>0))
+        Block[10].NAC1 += 1;
+    else if((b=='L')&&(r=="2AC")&&(Block[10].AC2>0))
+        Block[10].AC2 += 1;
+    else if((b=='L')&&(r=="2NAC")&&(Block[10].NAC2>0))
+        Block[10].NAC2 += 1;
+    else if((b=='L')&&(r=="3AC")&&(Block[10].AC3>0))
+        Block[10].AC3 += 1;
+    else if((b=='L')&&(r=="3NAC")&&(Block[10].NAC3>0))
+        Block[10].NAC3 += 1;
+    else if((b=='L')&&(r=="4AC")&&(Block[10].AC4>0))
+        Block[10].AC4 += 1;
+    else if((b=='L')&&(r=="4NAC")&&(Block[10].NAC4>0))
+        Block[10].NAC4 += 1;
+    else if((b=='L')&&(r=="6AC")&&(Block[10].AC6>0))
+        Block[10].AC6 += 1;
+    else if((b=='L')&&(r=="6NAC")&&(Block[10].NAC6>0))
+        Block[10].NAC6 += 1;
+     else if((b=='Q')&&(r=="1AC")&&(Block[11].AC1>0))
+        Block[11].AC1 += 1;
+    else if((b=='Q')&&(r=="1NAC")&&(Block[11].NAC1>0))
+        Block[11].NAC1 += 1;
+    else if((b=='Q')&&(r=="2AC")&&(Block[11].AC2>0))
+        Block[11].AC2 += 1;
+    else if((b=='Q')&&(r=="2NAC")&&(Block[11].NAC2>0))
+        Block[11].NAC2 += 1;
+    else if((b=='Q')&&(r=="3AC")&&(Block[11].AC3>0))
+        Block[11].AC3 += 1;
+    else if((b=='Q')&&(r=="3NAC")&&(Block[11].NAC3>0))
+        Block[11].NAC3 += 1;
+    else if((b=='Q')&&(r=="4AC")&&(Block[11].AC4>0))
+        Block[11].AC4 += 1;
+    else if((b=='Q')&&(r=="4NAC")&&(Block[11].NAC4>0))
+        Block[11].NAC4 += 1;
+    else if((b=='Q')&&(r=="6AC")&&(Block[11].AC6>0))
+        Block[11].AC6 += 1;
+    else if((b=='Q')&&(r=="6NAC")&&(Block[11].NAC6>0))
+        Block[11].NAC6 += 1;
